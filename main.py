69bfb0e8a867b2f0a2d0b814868fe27416834eb7
JellyPumps <88274478+JellyPumps@users.noreply.github.com>
2021-10-19 14:40:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from keep_alive import keep_alive
 client = discord.Client()
 
 commonWords = ['next', 'release', 'new', 'come', 'out']
+sentenceKeyWord = ['does', 'gonna', 'going']
 
 @client.event
 async def on_ready():
@@ -21,7 +22,7 @@ async def on_message(message):
   i = 0
   msgCont = message.content.lower()
 
-  if ('when' in msgCont) and ('chapter' in msgCont) or ('chap' in msgCont):
+  if ('when' in msgCont) and ('chapter' in msgCont):
     
     msgSplit = msgCont.split()
 
@@ -30,10 +31,10 @@ async def on_message(message):
     if len(msgSplit) > 2:  
       for word in commonWords:
         if word in msgSplit:
-          if "does" in msgSplit:
-            i = i + 1
-          if "gonna" in msgSplit:
-            i = i + 1
+          for sk in sentenceKeyWord:
+            if sk in msgSplit:
+              i = i + 1
+          
 
       print(i)
       if i > 0:
@@ -54,11 +55,52 @@ async def on_message(message):
 
     #await message.channel.send('Bot in testing mode, message has been recognized')  
   
+  #I got tried of trying to make it work :p
+  if message.content.startswith('when is chapter'):
+    opt = random.randint(1, 2)
+    if opt == 1:
+      await message.channel.send("When its done being translated :p")
+    if opt == 2:
+      await message.channel.send('When its ready to be released :p')
+
   if ('gib' in msgCont) and ('chapter' in msgCont):
     await message.channel.send("nay, thee wilt asketh nicely 'r as the ov'rl'rds has't did state +10 minutes to releaseth")
   
   if ('at' in msgCont) and ('which' in msgCont) and ('hour' in msgCont) and ("chapt'r" in msgCont):
     await message.channel.send("aft'r t done being did translate :p") 
+
+  #so it can work with chap
+  if ('when' in msgCont) and ('chap' in msgCont):
+    
+    msgSplit = msgCont.split()
+
+    print(msgSplit)
+
+    if len(msgSplit) > 2:  
+      for word in commonWords:
+        if word in msgSplit:
+          for sk in sentenceKeyWord:
+            if sk in msgSplit:
+              i = i + 1
+          
+
+      print(i)
+      if i > 0:
+        print("OK")
+        opt = random.randint(1, 2)
+        if opt == 1:
+          await message.channel.send("When its done being translated :p")
+        if opt == 2:
+          await message.channel.send('When its ready to be released :p')
+
+    else:
+      print("OK")
+      opt = random.randint(1, 2)
+      if opt == 1:
+        await message.channel.send("When its done being translated :p")
+      if opt == 2:
+        await message.channel.send('When its ready to be released :p')  
+
 
 keep_alive()
 client.run(os.environ['token'])
